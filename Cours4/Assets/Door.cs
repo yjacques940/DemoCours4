d64b2698e6af5ab39ba1acf853d7a55b5f829ee6
B533P-00\533 <agagnon@cegepba.qc.ca>
2018-08-31 15:00:26 -0400

    porte

---
+++ Cours4/Assets/Door.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Door : MonoBehaviour {
+    [SerializeField] string sceneName;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        ChangeLevel();
+    }
+
+    private void ChangeLevel()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
---
+++ Cours4/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField] public float maxSpeed = 7;
+    protected Vector2 targetVelocity;
+    protected Rigidbody2D rigidBody2D;
+    protected ContactFilter2D contactFilter;
+    protected RaycastHit2D[] hitBuffer = new RaycastHit2D[16];
+    protected List<RaycastHit2D> hitBufferList = new List<RaycastHit2D>(16);
+
+    protected const float minMoveDistance = 0.001f;
+    protected const float shellRadius = 0.01f;
+
+    private void Awake() {
+        contactFilter.useTriggers = false;
+        contactFilter.SetLayerMask(Physics2D.GetLayerCollisionMask(gameObject.layer));
+        contactFilter.useLayerMask = true;
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate() {
+        Vector2 velocityX = new Vector2(); ;
+        Vector2 velocityY = new Vector2(); ;
+        velocityX.x= targetVelocity.x;
+        velocityY.y = targetVelocity.y;
+        Vector2 deltaPositionX = velocityX * Time.deltaTime;
+        Movement(deltaPositionX, true);
+        Vector2 deltaPositionY = velocityY * Time.deltaTime;
+        Movement(deltaPositionY, true);
+    }
+    void Update() {
+        ComputeVelocity();
+    }
+    protected  void ComputeVelocity() {
+        Vector2 move = Vector2.zero;
+        move.x = Input.GetAxis("Horizontal");
+        move.y = Input.GetAxis("Vertical");
+        targetVelocity = move.normalized * maxSpeed;
+    }
+    void Movement(Vector2 move, bool yMovement) {
+        float distance = move.magnitude;
+
+        if (distance > minMoveDistance) {
+            int count = rigidBody2D.Cast(move, contactFilter, hitBuffer, distance + shellRadius);
+            hitBufferList.Clear();
+            for (int i = 0; i < count; i++) {
+                hitBufferList.Add(hitBuffer[i]);
+            }
+ 
+            for (int i = 0; i < hitBufferList.Count; i++) {
+                Vector2 currentNormal = hitBufferList[i].normal;
+                float modifiedDistance = hitBufferList[i].distance - shellRadius;
+                distance = modifiedDistance < distance ? modifiedDistance : distance;
+            }
+        }
+        rigidBody2D.position = rigidBody2D.position + move.normalized * distance;
+    }  
+}
