4ee0b3bdb3fb52ff3f9d5f7f7bc489b2980e6593
Yannick <yannick.jacques940@gmail.com>
2018-09-14 13:42:02 -0400

    Changer le script du playerMovement

--- Cours4/Assets/Script/PlayerMovement.cs
+++ Cours4/Assets/Script/PlayerMovement.cs
@@ -3,27 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
-
-
     private enum Direction { North, East, South, West}
     private Direction playerDirection = Direction.South;
     private Animator animator;
     [SerializeField] public float maxSpeed = 7;
     protected Vector2 targetVelocity;
-    protected Rigidbody2D rigidBody2D;
-    protected ContactFilter2D contactFilter;
-    protected RaycastHit2D[] hitBuffer = new RaycastHit2D[16];
-    protected List<RaycastHit2D> hitBufferList = new List<RaycastHit2D>(16);
-
+    protected Rigidbody2D playerRigidBody2D;
+    protected ContactFilter2D movementContactFilter;
+   
     protected const float minMoveDistance = 0.001f;
     protected const float shellRadius = 0.01f;
     private bool fireIsPressed = false;
+    private bool controlAreEnable = true;
 
     private void Awake() {
-        contactFilter.useTriggers = false;
-        contactFilter.SetLayerMask(Physics2D.GetLayerCollisionMask(gameObject.layer));
-        contactFilter.useLayerMask = true;
-        rigidBody2D = GetComponent<Rigidbody2D>();
+        movementContactFilter = BuildContactFilter2DForLayer(LayerMask.LayerToName(gameObject.layer));
+        playerRigidBody2D = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
     }
     void FixedUpdate() {
@@ -32,34 +27,33 @@ public class PlayerMovement : MonoBehaviour {
         velocityX.x= targetVelocity.x;
         velocityY.y = targetVelocity.y;
         Vector2 deltaPositionX = velocityX * Time.deltaTime;
-        Movement(deltaPositionX, true);
+        Movement(deltaPositionX);
         Vector2 deltaPositionY = velocityY * Time.deltaTime;
-        Movement(deltaPositionY, true);
+        Movement(deltaPositionY);
     }
     void Update() {
-        ComputeVelocity();
-        ManageInteraction();
-        
-    }
-
-    void UpdateAnimationSpeed(float speed) {
-        animator.SetFloat("Speed", speed);
-    }
-
-    void UpdateDirection(float movementX, float movementY) {
-        if (movementY > 0) {
-            playerDirection = Direction.North;
-        }else if (movementY < 0) {
-            playerDirection = Direction.South;
-        }else if (movementX > 0) {
-            playerDirection = Direction.East;
-        } else if (movementX < 0) {
-            playerDirection = Direction.West;
+        if (controlAreEnable) {
+            ProcessInput();
         }
-        animator.SetFloat("Direction", (float)playerDirection);
     }
 
-    protected  void ComputeVelocity() {
+    public void EnableControl() {
+        this.controlAreEnable = true;
+    }
+
+    public void DisableControl() {
+        this.controlAreEnable = false;
+        targetVelocity = Vector2.zero;
+    }
+
+    public void ProcessInput() {
+        if (controlAreEnable) {
+            ComputeVelocity();
+            ManageInteraction();
+        }
+    }
+
+    protected void ComputeVelocity() {
         Vector2 move = Vector2.zero;
         move.x = Input.GetAxis("Horizontal");
         move.y = Input.GetAxis("Vertical");
@@ -67,15 +61,13 @@ public class PlayerMovement : MonoBehaviour {
         UpdateDirection(move.x, move.y);
         UpdateAnimationSpeed(targetVelocity.magnitude);
     }
-    void Movement(Vector2 move, bool yMovement) {
-        float distance = move.magnitude;
 
+    private void Movement(Vector2 move) {
+        float distance = move.magnitude;
+        RaycastHit2D[] hitBuffer = new RaycastHit2D[16];
         if (distance > minMoveDistance) {
-            int count = rigidBody2D.Cast(move, contactFilter, hitBuffer, distance + shellRadius);
-            hitBufferList.Clear();
-            for (int i = 0; i < count; i++) {
-                hitBufferList.Add(hitBuffer[i]);
-            }
+            int movementColisionHitCount = playerRigidBody2D.Cast(move, movementContactFilter, hitBuffer, distance + shellRadius);
+            List<RaycastHit2D> hitBufferList = BufferArrayHitToList(hitBuffer, movementColisionHitCount);
  
             for (int i = 0; i < hitBufferList.Count; i++) {
                 Vector2 currentNormal = hitBufferList[i].normal;
@@ -83,41 +75,60 @@ public class PlayerMovement : MonoBehaviour {
                 distance = modifiedDistance < distance ? modifiedDistance : distance;
             }
         }
-        rigidBody2D.position = rigidBody2D.position + move.normalized * distance;
+        playerRigidBody2D.position = playerRigidBody2D.position + move.normalized * distance;
     }
 
     private void ManageInteraction()
     {
-        if (Input.GetAxis("Jump") != 0)
-        {
-            if (!fireIsPressed)
-            {
-                print("fire");
+        if (Input.GetAxis("Jump") != 0){
+            if (!fireIsPressed) {
                 fireIsPressed = true;
-                ContactFilter2D contactFilter2DInteraction = new ContactFilter2D();
-                contactFilter2DInteraction.useTriggers = false;
-
-                contactFilter2DInteraction.SetLayerMask(Physics2D.GetLayerCollisionMask(LayerMask.NameToLayer("Interaction")));
-                contactFilter2DInteraction.useLayerMask = true;
+                ContactFilter2D contactFilter2DInteraction = BuildContactFilter2DForLayer("Interaction");
                 RaycastHit2D[] interactionHit = new RaycastHit2D[16];
-                List<RaycastHit2D> hitBufferListInteraction = new List<RaycastHit2D>(16);
-                hitBufferListInteraction.Clear();
-                int countInteraction = Physics2D.Raycast(gameObject.transform.position, Vector2.up, contactFilter2DInteraction, interactionHit);
-                print(countInteraction);
-                for (int i = 0; i < countInteraction; i++)
-                {
-                    hitBufferListInteraction.Add(interactionHit[i]);
-                }
-                if (hitBufferListInteraction.Count > 0)
-                {
+                int interactionCollisionHitCount = Physics2D.Raycast(gameObject.transform.position, Vector2.up, contactFilter2DInteraction, interactionHit);
+                List<RaycastHit2D> hitBufferListInteraction = BufferArrayHitToList(interactionHit, interactionCollisionHitCount);
+                if (hitBufferListInteraction.Count > 0){
                     hitBufferListInteraction[0].transform.gameObject.GetComponent<Interaction>().Interact();
                 }
             }
         }
-        else
-            {
-                fireIsPressed = false;
-            }
+        else {
+            fireIsPressed = false;
+        }
         
+    }
+
+    private void UpdateAnimationSpeed(float speed) {
+        animator.SetFloat("Speed", speed);
+    }
+
+    private void UpdateDirection(float movementX, float movementY) {
+        if (movementY > 0) {
+            playerDirection = Direction.North;
+        } else if (movementY < 0) {
+            playerDirection = Direction.South;
+        } else if (movementX > 0) {
+            playerDirection = Direction.East;
+        } else if (movementX < 0) {
+            playerDirection = Direction.West;
+        }
+        animator.SetFloat("Direction", (float)playerDirection);
+    }
+
+    private ContactFilter2D BuildContactFilter2DForLayer(string LayerName) {
+        ContactFilter2D contactFilter2DInteraction = new ContactFilter2D();
+        contactFilter2DInteraction.useTriggers = false;
+        contactFilter2DInteraction.SetLayerMask(Physics2D.GetLayerCollisionMask(LayerMask.NameToLayer(LayerName)));
+        contactFilter2DInteraction.useLayerMask = true;
+        return contactFilter2DInteraction;
+    }
+
+    private List<RaycastHit2D> BufferArrayHitToList(RaycastHit2D[] hitBuffer, int count) {
+        List<RaycastHit2D> hitBufferList = new List<RaycastHit2D>(count);
+        hitBufferList.Clear();
+        for (int i = 0; i < count; i++) {
+            hitBufferList.Add(hitBuffer[i]);
+        }
+        return hitBufferList;
     }
 }
